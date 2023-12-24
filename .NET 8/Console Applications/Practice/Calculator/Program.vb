83b41efbd7ae723f2d35ce45d8381f35add73150
Rob C <caleror1083@gmail.com>
2023-12-24 17:33:05 -0500

    Updated apps

--- .NET 8/Console Applications/Practice/Calculator/Program.vb
+++ .NET 8/Console Applications/Practice/Calculator/Program.vb
@@ -1,8 +1,21 @@
+'Calculator - Visual Basic Console Application
 Imports System
 
 Friend Module Program
     Public Sub Main()
-        Console.Write("O")
+        Dim number1 As Integer
+        Dim number2 As Integer
+        Dim product As Integer
+
+        Console.Write("Enter the first number: ")
+        number1 = Console.ReadLine()
+
+        Console.Write("Enter the second number: ")
+        number2 = Console.ReadLine()
+
+        product = number1 * number2
+
+        Console.Write("The answer is: " & product)
         Console.ReadKey()
     End Sub
 End Module
